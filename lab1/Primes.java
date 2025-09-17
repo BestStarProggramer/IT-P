1a28e4c57396661ff11b20dd1563ab3da2eb08fd
Dmitriy <3sheepcanfly3@gmail.com>
2025-09-17 11:31:44 +0300

    lab1 added

---
+++ lab1/Primes.java
@@ -0,0 +1,23 @@
+public class Primes {
+    public static void main(String[] args) {
+        int limit = 100; 
+        System.out.println("Prime numbers up to " + limit + ":");
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+     }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+}
+}
